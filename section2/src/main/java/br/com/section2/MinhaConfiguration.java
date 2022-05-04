88bbfe177005f4706e2cbffd4c88952ca9f82a45
FelipeArruda15 <souzafelipesouza5@gmail.com>
2022-05-04 19:31:16 -0300

    Configuração externalizada: Application Properties

--- section2/src/main/java/br/com/section2/MinhaConfiguration.java
+++ section2/src/main/java/br/com/section2/MinhaConfiguration.java
@@ -5,8 +5,4 @@ import org.springframework.context.annotation.*;
 @Configuration
 public class MinhaConfiguration {
 
-    @Bean(name = "applicationName")
-    public String applicationName(){
-        return "Sistema de Vendas";
     }
-}
--- section2/src/main/java/br/com/section2/Section2Application.java
+++ section2/src/main/java/br/com/section2/Section2Application.java
@@ -9,8 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class Section2Application {
 
-	@Autowired
-	@Qualifier("applicationName")
+	@Value("${application.name}")
 	private String applicationName;
 
 	@GetMapping("/hello")
