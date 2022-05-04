90a5f545a35ac8b98512e2858ea7362caa3442b8
FelipeArruda15 <souzafelipesouza5@gmail.com>
2022-05-04 19:39:17 -0300

    Trabalhando com multiplos ambientes e profiles

--- section2/src/main/java/br/com/section2/MinhaConfiguration.java
+++ section2/src/main/java/br/com/section2/MinhaConfiguration.java
@@ -1,8 +1,16 @@
 package br.com.section2;
 
+import org.springframework.boot.*;
 import org.springframework.context.annotation.*;
 
 @Configuration
+@Profile("development")
 public class MinhaConfiguration {
 
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("Rodando a configuração de desenvolvimento");
+        };
     }
+}
