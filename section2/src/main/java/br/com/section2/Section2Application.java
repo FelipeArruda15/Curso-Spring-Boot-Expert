f2891ff9dc33d16361f12439bc880f6cb197416a
FelipeArruda15 <souzafelipesouza5@gmail.com>
2022-05-03 23:25:52 -0300

    Hello World com Spring Boot

--- section2/src/main/java/br/com/section2/Section2Application.java
+++ section2/src/main/java/br/com/section2/Section2Application.java
@@ -2,9 +2,16 @@ package br.com.section2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@RestController
 public class Section2Application {
+
+	@GetMapping("/hello")
+	public String helloWorld(){
+		return "Hello World!!";
+	}
 
 	public static void main(String[] args) {
 
