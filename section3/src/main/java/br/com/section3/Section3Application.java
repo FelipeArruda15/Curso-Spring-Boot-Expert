3b3f4b97e2b31289d7a80cff05c00fd4fab0f8db
FelipeArruda15 <souzafelipesouza5@gmail.com>
2022-05-07 20:14:06 -0300

    Salvando e recuperando clientes com JDBCTemplate

--- section3/src/main/java/br/com/section3/Section3Application.java
+++ section3/src/main/java/br/com/section3/Section3Application.java
@@ -1,10 +1,27 @@
 package br.com.section3;
 
+import br.com.section3.domain.entity.Cliente;
+import br.com.section3.repository.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Section3Application {
+
+	@Bean
+	public CommandLineRunner init(@Autowired Clientes clientes){
+		return args -> {
+			clientes.salvar(new Cliente("Felipe"));
+			clientes.salvar(new Cliente("Daniela"));
+			List<Cliente> todosClientes = clientes.obterTodos();
+			todosClientes.forEach(System.out::println);
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Section3Application.class, args);
