be82d62aec6a61cfab2f34e507e8c840ffb088f3
FelipeArruda15 <souzafelipesouza5@gmail.com>
2022-05-16 10:33:19 -0300

    Identando a classe Produto

--- section3/src/main/java/br/com/section3/domain/entity/Produto.java
+++ section3/src/main/java/br/com/section3/domain/entity/Produto.java
@@ -11,8 +11,10 @@ public class Produto {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private  Integer id;
+
     @Column(name = "descricao")
     private  String descricao;
+
     @Column(name = "preco_unitario")
     private BigDecimal preco;
 
