966152d1e0b25e6c164c3c3c05102a15d08b7aeb
FelipeArruda15 <souzafelipesouza5@gmail.com>
2022-05-16 22:28:53 -0300

    Requisição DELETE

--- API RESTFUL/src/main/java/br/com/section4/rest/controller/ClienteController.java
+++ API RESTFUL/src/main/java/br/com/section4/rest/controller/ClienteController.java
@@ -34,4 +34,16 @@ public class ClienteController {
         Cliente clienteSalvo = clientes.save(cliente);
         return ResponseEntity.ok(clienteSalvo);
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable Integer id){
+        Optional<Cliente> cliente = clientes.findById(id);
+
+        if(cliente.isPresent()){
+            clientes.delete(cliente.get());
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.notFound().build();
+    }
 }
