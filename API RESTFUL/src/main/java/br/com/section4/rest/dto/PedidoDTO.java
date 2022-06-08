dcab77fb9390b59e7c563e7a945339e45e19125c
FelipeArruda15 <souzafelipesouza5@gmail.com>
2022-06-07 22:29:45 -0300

    Validando a entidade de Pedido

--- API RESTFUL/src/main/java/br/com/section4/rest/dto/PedidoDTO.java
+++ API RESTFUL/src/main/java/br/com/section4/rest/dto/PedidoDTO.java
@@ -1,11 +1,15 @@
 package br.com.section4.rest.dto;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.List;
 
 public class PedidoDTO {
 
+    @NotNull(message = "Informe o código do cliente.")
     private Integer cliente;
+    @NotNull(message = "Campo Total do pedido é obrigatório.")
     private BigDecimal total;
     private List<ItemPedidoDTO> items;
 
