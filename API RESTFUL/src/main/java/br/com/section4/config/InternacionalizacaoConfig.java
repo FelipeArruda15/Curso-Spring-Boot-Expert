a2932a89757a3aebb8ea407f9b17c15078e5c5fb
FelipeArruda15 <souzafelipesouza5@gmail.com>
2022-06-08 22:12:35 -0300

    Trabalhando com internacionalização(Messagens resources)

---
+++ API RESTFUL/src/main/java/br/com/section4/config/InternacionalizacaoConfig.java
@@ -0,0 +1,37 @@
+package br.com.section4.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import java.util.Locale;
+
+@Configuration
+public class InternacionalizacaoConfig {
+
+    /**
+     * Fonte de Mensagens
+     * @return
+     */
+    @Bean
+    public MessageSource messageSource(){
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("ISO-8859-1");
+        messageSource.setDefaultLocale(Locale.getDefault());
+        return messageSource;
+    }
+
+    /**
+     * Objeto responsavel por fazer a interpolação(pegar a chave e trocar pelo valor)
+     * @return
+     */
+    @Bean
+    public LocalValidatorFactoryBean validatorFactoryBean(){
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
+}
