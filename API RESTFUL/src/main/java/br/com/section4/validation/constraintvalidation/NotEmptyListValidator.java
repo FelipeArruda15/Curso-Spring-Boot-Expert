96dd39b689b2dd89cf13a69b9855228014c504e6
FelipeArruda15 <souzafelipesouza5@gmail.com>
2022-06-08 21:40:53 -0300

    2 - Criando anotação customizada para validar que pedidos venham com pelo menos 1 item.

---
+++ API RESTFUL/src/main/java/br/com/section4/validation/constraintvalidation/NotEmptyListValidator.java
@@ -0,0 +1,20 @@
+package br.com.section4.validation.constraintvalidation;
+
+import br.com.section4.validation.NotEmptyList;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.List;
+
+public class NotEmptyListValidator implements ConstraintValidator<NotEmptyList, List> {
+
+    @Override
+    public void initialize(NotEmptyList constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(List list, ConstraintValidatorContext constraintValidatorContext) {
+        return list != null && !list.isEmpty();
+    }
+
+}
